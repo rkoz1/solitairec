511e6e224ccf16632b44e7d3e6e67ea2cbbe1211
Rafal Koziarz <5444501+rkoz1@users.noreply.github.com>
2026-04-03 15:50:54 +0800

    feat: update settings and remove unused rewrites in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return {
-      beforeFiles: [],
-      afterFiles: [
-        {
-          source: "/_api/:path*",
-          destination:
-            "https://dd4ab6dc-0e94-4aee-8a2e-b0320758fca1.siteproxy.wix.com/_api/:path*",
-        },
-        {
-          source: "/__ecom/:path*",
-          destination:
-            "https://dd4ab6dc-0e94-4aee-8a2e-b0320758fca1.siteproxy.wix.com/__ecom/:path*",
-        },
-      ],
-      fallback: [],
-    };
-  },
   images: {
     remotePatterns: [
       {
